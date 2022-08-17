08365526e99ef5ca1eb76f1d1f94b52fafadea3a
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2022-08-17 14:49:10 -0400

    [fix] Clean category.

--- BlazorECommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorECommerce/Client/Services/ProductService/ProductService.cs
@@ -18,7 +18,7 @@ public class ProductService : IProductService
 
     public async Task GetProducts(string? categoryUrl = null)
     {
-        var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product{(categoryUrl == null ? string.Empty : $"/category/{categoryUrl}")}");
+        var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/{(categoryUrl == null ? string.Empty : $"/category/{categoryUrl}")}");
         if (result != null && result.Data != null)
         {
             Products = result.Data;
