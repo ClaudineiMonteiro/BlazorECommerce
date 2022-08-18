128a431b1645b1443ebabc77d7e5d2c4f947b974
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2022-08-18 15:06:56 -0400

    [add] Pagination

--- BlazorECommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorECommerce/Server/Services/ProductService/ProductService.cs
@@ -98,11 +98,25 @@ public class ProductService : IProductService
         return new ServiceResponse<List<string>> { Data = result };
     }
 
-    public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+    public async Task<ServiceResponse<ProductSearchResult>> SearchProducts(string searchText, int page)
     {
-        var response = new ServiceResponse<List<Product>>
+        var pageResult = 2f;
+        var pageCount = Math.Ceiling((await FindProductsBySearchText(searchText)).Count / pageResult);
+        var products = await _context.Products.Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+                    ||
+                    p.Description.ToLower().Contains(searchText.ToLower()))
+                    .Include(v => v.Variants)
+                    .Skip((page - 1) * (int)pageResult)
+                    .Take((int)pageResult)
+                    .ToListAsync();
+        var response = new ServiceResponse<ProductSearchResult>
         {
-            Data = await FindProductsBySearchText(searchText)
+            Data = new ProductSearchResult
+            {
+                Products = products,
+                CurretPage = page,
+                Pages = (int)pageCount
+            }
         };
 
         return response;
