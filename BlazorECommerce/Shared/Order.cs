f3f8eb38f348a2c1de1776c64c4c8eb2fd0ccf88
ClaudineiMonteiro <claudinei.monteiro@outlook.com>
2022-08-25 12:48:57 -0400

    [add] Order in server side.

--- BlazorECommerce/Shared/Order.cs
+++ BlazorECommerce/Shared/Order.cs
@@ -6,7 +6,7 @@ public class Order
 {
     public int Id { get; set; }
     public int UserId { get; set; }
-    public DateTime dateTime { get; set; } = DateTime.Now;
+    public DateTime OrderDate { get; set; } = DateTime.Now;
     [Column(TypeName = "decimal(18,2)")]
     public decimal TotalPrice { get; set; }
     public List<OrderItem> OrderItems { get; set; }
